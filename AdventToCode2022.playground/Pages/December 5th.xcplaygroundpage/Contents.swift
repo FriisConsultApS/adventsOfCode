a6b995220e49722d394392b73d3d1b4fed38db0f
Per Friis <per.friis@friisconsult.com>
2022-12-05 15:17:44 +0100

    December 5th, my solution

--- AdventToCode2022.playground/Pages/December 5th.xcplaygroundpage/Contents.swift
+++ AdventToCode2022.playground/Pages/December 5th.xcplaygroundpage/Contents.swift
@@ -1,14 +1,69 @@
 //: [Previous](@previous)
 
 import Foundation
-guard let filePath = Bundle.main.path(forResource: "demo", ofType: "txt") else { fatalError("No file") }
+guard let filePath = Bundle.main.path(forResource: "data", ofType: "txt") else { fatalError("No file") }
 
 do {
     let file = try String(contentsOfFile: filePath, encoding: .utf8)
     let lines = file.components(separatedBy: .newlines)
     var stackLines = [String]()
 
+    for line in lines {
+        stackLines.append(line)
+        if line.isEmpty { break }
+    }
 
+    var stacks = Array(repeating: Array<Character>(), count: 9)
+    for row in stackLines {
+        print(row)
+        for index in stacks.indices {
+            let colIndex = (index * 4) + 1
+            if row.count >= colIndex {
+                let element = row[row.index(row.startIndex, offsetBy: colIndex)]
+                if element.isLetter {
+                    stacks[index].append(element)
+                }
+
+            }
+        }
+    }
+
+
+
+    guard let movesIndexFrom = lines.firstIndex(where: {$0.isEmpty}) else { fatalError( "No moves")}
+    for stack in stacks {
+        print(stack)
+    }
+
+
+    for move in lines[movesIndexFrom...] {
+        let values =  move.components(separatedBy: .whitespaces)
+        guard values.count == 6  else { continue }
+
+        if let moveCount = Int(values[1]),
+           let fromContainer = Int(values[3]),
+           let toContainer = Int(values[5]) {
+
+            print("move \(moveCount) from \(fromContainer) to \(toContainer)")
+            guard moveCount > 0 else { continue }
+            stacks[toContainer-1].insert(contentsOf: stacks[fromContainer-1][0..<moveCount], at: 0)
+            for _ in 0..<moveCount {
+                guard !stacks[fromContainer - 1].isEmpty else { continue }
+                let element = stacks[fromContainer-1].removeFirst()
+            //    stacks[toContainer-1].insert(element, at: 0)
+            }
+            for stack in stacks {
+                print(stack)
+            }
+
+        }
+    }
+
+    print(stacks.compactMap({$0.first}).compactMap({String($0)}).joined())
+
+
+
+} catch {
 
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # adventsOfCode
+This is just my "dirty way of solving the [Advent Of Code](https://adventofcode.com) challenge
+
+This is only for fun, coding style and architecture is not a representation of my work, this is created for speed and fun.
